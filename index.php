078b313418c12f29487356fadc5f5fd57fe52394
pako <pako@192.168.0.10>
2015-01-27 00:58:59 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//2015-01-27 12:35 AM
+?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Angular WishList</title>
+    </head>
+    <body>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.24/angular.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.24/angular-route.js"></script>
+        <script src="js/wishList.js"></script>
+    </body>
+</html>
---
+++ js/wishList.js
@@ -0,0 +1,11 @@
+!function (){
+    'use strict';
+    //<editor-fold defaultstate="collapsed" desc="Angular App">
+    var wishListApp = angular.module('wishListApp');
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Controller">
+    var mainCtrl = wishListApp.controller('mainCtrl');
+    //</editor-fold>
+    
+    
+}();

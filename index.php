9e3b762853e241165be704c4f034d36025a4fa61
pako <pako@192.168.0.10>
2015-01-27 01:50:54 -0600

    created views and init routing.

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 //2015-01-27 12:35 AM
 ?><!DOCTYPE html>
-<html>
+<html ng-app="wishListApp" >
     <head>
         <meta charset="UTF-8">
         <title>Angular WishList</title>
     </head>
-    <body>
+    <body ng-controller="mainCtrl as vm">
+        <nav>
+            <ul>
+                <li><a href="#/">Budgeti</a></li>
+                <li><a href="#/wishList">My wishlist items</a></li>
+                <li><a href="#/boughtItems">My bought items</a></li>
+            </ul>
+        </nav>
+        <div ng-view=""></div>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.24/angular.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.24/angular-route.js"></script>
         <script src="js/wishList.js"></script>
--- js/wishList.js
+++ js/wishList.js
@@ -1,11 +1,31 @@
 !function (){
     'use strict';
     //<editor-fold defaultstate="collapsed" desc="Angular App">
-    var wishListApp = angular.module('wishListApp');
-    //</editor-fold>
-    //<editor-fold defaultstate="collapsed" desc="Controller">
-    var mainCtrl = wishListApp.controller('mainCtrl');
+    var wishListApp = angular.module('wishListApp', ["ngRoute"]);
+    wishListApp.config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                controller: "mainCtrl",
+                controllerAs: "vm",
+                templateUrl: "views/budget.html"
+            })
+            .when("/wishList", {
+                controller: "mainCtrl",
+                controllerAs: "vm",
+                templateUrl: "views/boughtItems.html"
+            })
+            .when("/boughtItems", {
+                controller: "mainCtrl",
+                controllerAs: "vm",
+                templateUrl: "views/wishList.html"
+            });
+    });
     //</editor-fold>
     
+    //<editor-fold defaultstate="collapsed" desc="Controller">
+    var mainCtrl = wishListApp.controller('mainCtrl', function ($scope) {
+        this.algo = 'xD';
+    });
+    //</editor-fold>
     
 }();

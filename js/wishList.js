52d62a370e634f23e273db342f3d353ea721b7fc
pako <pako@192.168.0.10>
2015-01-27 03:42:23 -0600

    create onReadFile directive

--- js/wishList.js
+++ js/wishList.js
@@ -10,12 +10,12 @@
                         controllerAs: "vm",
                         templateUrl: "views/budget.html"
                     })
-                    .when("/wishList", {
+                    .when("/boughtItems", {
                         controller: "mainCtrl",
                         controllerAs: "vm",
                         templateUrl: "views/boughtItems.html"
                     })
-                    .when("/boughtItems", {
+                    .when("/wishList", {
                         controller: "mainCtrl",
                         controllerAs: "vm",
                         templateUrl: "views/wishList.html"
@@ -40,15 +40,44 @@
             });
         //</editor-fold>
 
-    
+        //<editor-fold defaultstate="collapsed" desc="create onReadFile">
+        wishListApp.directive('onReadFile', function ($parse) {
+            return {
+		restrict: 'A',
+		scope: false,
+		link: function (scope, element, attrs) {
+                    var fn = $parse(attrs.onReadFile);
+                    element.on('change', function (onChangeEvent) {
+                        var reader = new FileReader();
+                        reader.onload = function (onLoadEvent) {
+                            scope.$apply(function () {
+                                fn(scope, {
+                                    $fileContent : onLoadEvent.target.result
+                                });
+                            });
+                        };
+                        reader.readAsDataURL((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+                    });
+		}
+	};
+});
+        //</editor-fold>
+
     //</editor-fold>
     
     //<editor-fold defaultstate="collapsed" desc="Controller">
     var mainCtrl = wishListApp.controller('mainCtrl', function (factory) {
         this.budgetLimit =  factory.getBudgetLimit();
         var $scope = this;
+        $scope.file = 10;
         this.setBudgetLimit = function () {
             factory.setBudgetLimit($scope.budgetLimit);
+        };
+        this.getFile = function () {
+            console.log('bind', $scope.file);
+        };
+        this.showContent = function (img) {
+           $scope.fileTem = img;
         };
     });
     //</editor-fold>

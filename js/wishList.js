0180228b3a91edbda9e418820c92cf86d6bb8b29
pako <pako@192.168.0.10>
2015-01-27 02:36:51 -0600

    The getBudgetLimit and setBdgetLimit methods were added to the factory

--- js/wishList.js
+++ js/wishList.js
@@ -2,29 +2,54 @@
     'use strict';
     //<editor-fold defaultstate="collapsed" desc="Angular App">
     var wishListApp = angular.module('wishListApp', ["ngRoute"]);
-    wishListApp.config(function ($routeProvider) {
-        $routeProvider
-            .when("/", {
-                controller: "mainCtrl",
-                controllerAs: "vm",
-                templateUrl: "views/budget.html"
-            })
-            .when("/wishList", {
-                controller: "mainCtrl",
-                controllerAs: "vm",
-                templateUrl: "views/boughtItems.html"
-            })
-            .when("/boughtItems", {
-                controller: "mainCtrl",
-                controllerAs: "vm",
-                templateUrl: "views/wishList.html"
+        //<editor-fold defaultstate="collapsed" desc="init Routing">
+            wishListApp.config(function ($routeProvider) {
+                $routeProvider
+                    .when("/", {
+                        controller: "mainCtrl",
+                        controllerAs: "vm",
+                        templateUrl: "views/budget.html"
+                    })
+                    .when("/wishList", {
+                        controller: "mainCtrl",
+                        controllerAs: "vm",
+                        templateUrl: "views/boughtItems.html"
+                    })
+                    .when("/boughtItems", {
+                        controller: "mainCtrl",
+                        controllerAs: "vm",
+                        templateUrl: "views/wishList.html"
+                    });
             });
-    });
+        //</editor-fold>
+        //<editor-fold defaultstate="collapsed" desc="init Factory">
+            wishListApp.factory('factory', function () {
+                var data = {
+                    budgetLimit : 30
+                };
+                var methods = {
+                    getBudgetLimit : function () {
+                        return data.budgetLimit;
+                    },
+                    setBudgetLimit : function (newLimit) {
+                        data.budgetLimit = parseInt(newLimit, 10);
+                        return this;
+                    }
+                };
+                return methods;
+            });
+        //</editor-fold>
+
+    
     //</editor-fold>
     
     //<editor-fold defaultstate="collapsed" desc="Controller">
-    var mainCtrl = wishListApp.controller('mainCtrl', function ($scope) {
-        this.algo = 'xD';
+    var mainCtrl = wishListApp.controller('mainCtrl', function (factory) {
+        this.budgetLimit =  factory.getBudgetLimit();
+        var $scope = this;
+        this.setBudgetLimit = function () {
+            factory.setBudgetLimit($scope.budgetLimit);
+        };
     });
     //</editor-fold>
     

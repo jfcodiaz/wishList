8825b3d88362bef72061773ab40bda11e203feac
pako <pako@192.168.0.10>
2015-01-27 20:32:38 -0600

    add jquery library

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-//2015-01-27 12:35 AM
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html ng-app="wishListApp" >
     <head>
         <meta charset="UTF-8">
@@ -9,13 +7,15 @@
     <body ng-controller="mainCtrl as vm">
         <nav>
             <ul>
-                <li><a href="#/">Budgeti</a></li>
+                <li><a href="#/">Budget</a></li>
                 <li><a href="#/wishList">My wishlist items</a></li>
                 <li><a href="#/boughtItems">My bought items</a></li>
             </ul>
         </nav>
-        <div ng-view=""></div>
+        <div ng-view class="reveal-animation"></div>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.24/angular.min.js"></script>
+        <script src ="http://code.angularjs.org/1.2.0-rc.2/angular-animate.js" type="text/javascript" charset="utf-8"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.24/angular-route.js"></script>
         <script src="js/wishList.js"></script>
     </body>

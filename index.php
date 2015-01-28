c1809d2d1dd2b72911e9419f76a4dea99e471df4
pako <pako@192.168.0.10>
2015-01-28 04:17:14 -0600

    Final

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
         <title>Angular WishList</title>
         <link href="css/styles.css" rel="stylesheet" type="text/css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--[if gte IE 9]>
+        <style type="text/css">
+          .gradient {
+             filter: none;
+          }
+        </style>
+      <![endif]-->
     </head>
     <body ng-controller="mainCtrl as vm">
         <nav>
@@ -14,10 +21,9 @@
                 <li><a href="#/boughtItems">My bought items</a></li>
             </ul>
         </nav>
-        <div ng-view class="reveal-animation"></div>
+        <div ng-view class="content"></div>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.24/angular.min.js"></script>
-        <script src ="http://code.angularjs.org/1.2.0-rc.2/angular-animate.js" type="text/javascript" charset="utf-8"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.24/angular-route.js"></script>
         <script src="js/wishList.js"></script>
     </body>
